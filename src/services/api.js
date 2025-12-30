f28a0ffee890362da7e8f1e78c6dbea0aab97718
ilucasbrandao <dev.lucasbrandao@gmail.com>
2025-12-30 14:49:12 -0300

    ajuste da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: import.meta.env.VITE_API_URL, // seu backend escolar
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000" // seu backend escolar
+
 });
 
 api.interceptors.request.use((config) => {
